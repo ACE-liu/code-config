35e7041c01991c58411e356af7392dcbf5cd499f
ACE-liu <782202048@qq.com>
2019-12-10 14:35:11 +0800

    添加自动接听参数

--- linphone/run.sh
+++ linphone/run.sh
@@ -5,4 +5,4 @@ runPath=`pwd`
 export LD_LIBRARY_PATH=$runPath/lib:$runPath/lib/mediastreamer/plugins:$LD_LIBRARY_PATH
 
 
-$runPath/bin/linphonec -d 2 -c $runPath/linphonerc
+$runPath/bin/linphonec -a -d 2 -c $runPath/linphonerc
